7c606d7354c4441b1091e2905e6bd86e7f1040a6
WakuwakuP <naoki.fujisawa@wakuwakup.net>
2019-07-18 15:20:35 +0900

    test

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -1,7 +1,8 @@
 <template>
   <footer class="footer">
     <button v-if="isLogin" class="button button--link" @click="logout">Logout</button>
-    <RouterLink v-else class="button button--link" to="/login">Login / Register</RouterLink>
+    <RouterLink v-else class="button button--link" to="/login">Login /Register</RouterLink>
+    <div>わーーーー</div>
   </footer>
 </template>
 
